6553274e731c90b31ccabcee7aba0ec143b32d37
Erik119 <erik@u2us.nl>
2020-08-10 20:47:02 +0200

    update

---
+++ logger/logger.py
@@ -0,0 +1,13 @@
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s %(levelname)s - %(message)s')
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+
+logger.debug('Hi Erik')
+
